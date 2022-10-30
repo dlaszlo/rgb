99020ad6fb51517be96cca0ce1be39407340135a
David Laszlo <laszlo.david@gmail.com>
2022-10-30 23:39:09 +0100

    fire effect

--- rgb.py
+++ rgb.py
@@ -4,19 +4,16 @@ from screen import Screen
 
 screen = Screen(800, 500)
 screen_mem1 = [0 for i in range(1040)]
-screen_mem2 = [0 for i in range(1040)]
 
-for i in range(42):
-    screen.set_palette(i, (i * 6, 0, 0))
-    screen.set_palette(i + 42, (255, i * 6, 0))
-    screen.set_palette(i + 84, (255, 255, i * 6))
-screen.set_palette(126, (255, 255, 255))
-screen.set_palette(127, (255, 255, 255))
+for i in range(32):
+    screen.set_palette(i, (i * 8, 0, 0))
+    screen.set_palette(i + 32, (255, i * 8, 0))
+    screen.set_palette(i + 64, (255, 255, i * 8))
+    screen.set_palette(i + 96, (255, 255, 255))
 
 screen.save_palette("pattern_256x1.png")
 
 ii = 0
-# random.shuffle(pos)
 
 while screen.check_quit():
 
@@ -25,7 +22,7 @@ while screen.check_quit():
             screen_mem1[i] = random.randint(0, 127)
     ii += 1
 
-    for p in range(400, 1000):
+    for p in range(40, 1000):
         if p % 2 == 0:
             screen_mem1[p] = (screen_mem1[p - 40] + screen_mem1[p + 40]) >> 1
         else:
